10d04532b4a5974ecd22f08b150f93ffdaf872c4
erhantopcu <erhn.topcu@hotmail.com>
2022-12-30 14:18:14 +0300

    controllers-finished

---
+++ MvcKutuphane1/Controllers/islemController.cs
@@ -0,0 +1,20 @@
+﻿using MvcKutuphane1.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcKutuphane1.Controllers
+{
+    public class islemController : Controller
+    {
+        DBKUTUPHANEEntities1 db = new DBKUTUPHANEEntities1();
+        // GET: islem
+        public ActionResult Index()
+        {
+            var degerler = db.TBLHAREKET.Where(x => x.ISLEMDURUM == true).ToList();
+            return View(degerler);
+        }
+    }
+}
